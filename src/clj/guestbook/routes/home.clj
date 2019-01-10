87c9e1f65271afa37bbac86291e631ba19e54ee9
Karol Wieloch <kwieloch@gmail.com>
2019-01-10 10:15:40 +0100

    Add validation

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -2,23 +2,43 @@
   (:require [guestbook.layout :as layout]
             [guestbook.db.core :as db]
             [compojure.core :refer [defroutes GET POST]]
-            [ring.util.http-response :as response]))
+            [ring.util.http-response :as response]
+            [struct.core :as st]))
 
-(defn home-page []
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+
+   [:message
+    st/required
+    st/string
+    {:message "message must contain at least 10 characters"
+     :validate #(> (count %) 9)}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! (assoc params 
+                               :timestamp (java.util.Date.)))
+      (response/found "/"))))
+
+(defn home-page [{:keys [flash]}]
   (layout/render
     "home.html"
-    {:messages (db/get-messages)}))
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
-(defn save-message! [{:keys [params]}]
-  (db/save-message! (assoc params 
-                           :timestamp (java.util.Date.)))
-  (response/found "/"))
-
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page))
-  (POST "/message" request (save-message! request)))
+  (GET "/" request (home-page request))
+  (POST "/message" request (save-message! request))  
+  (GET "/about" [] (about-page)))
 
