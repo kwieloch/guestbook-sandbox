fe79de314c26ff395c580c4466fa713bcf468481
Karol Wieloch <kwieloch@gmail.com>
2019-01-11 14:26:53 +0100

    render form by reagent

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -16,6 +16,23 @@
       [:p message]
       [:p "--- " name]])])
 
+(defn message-form []
+  (let [fields (reagent/atom nil)]
+    (fn []
+      [:div.content [:div.form-group
+                     [:p "name: " (:name @fields)]
+                     [:p "message: " (:message @fields)]
+                     [:p "Name"] [:input.form-control {:type :text
+                                                       :name :name
+                                                       :value (:name @fields)
+                                                       :on-change #(swap! fields assoc :name (-> % .-target .-value) )}]
+                     [:p "Message"] [:textarea.form-control {:rows 4 :cols 50
+                                                             :name :message
+                                                             :value (:message @fields)
+                                                             :on-change #(swap! fields assoc :message (-> % .-target .-value) )}]
+                     [:input.btn.btn-primary {:type :submit :value "Comment"}]]])))
+
+
 (defn home []
   (let [messages (reagent/atom nil)]
     (get-messages messages)
@@ -23,6 +40,7 @@
       [:div
        [:div.row
         [:div.span12 [message-list messages]]]
+       [:div.row [:div.span12 [message-form]]]
        [:h1 "Hello World from Reagent"]])))
 
 (reagent/render
