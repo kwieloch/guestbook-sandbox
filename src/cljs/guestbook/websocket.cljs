64d9c0c9f353083dfe968fc30ecb93b5b92c0657
Karol Wieloch <kwieloch@gmail.com>
2019-01-20 15:33:42 +0100

    use sente functions from example (client/server); refactor; reformat

--- src/cljs/guestbook/websocket.cljs
+++ src/cljs/guestbook/websocket.cljs
@@ -9,15 +9,18 @@
   (.log js/console "CSRF token detected in HTML, great!")
   (.log js/console "CSRF token NOT detected in HTML, default Sente config will reject requests"))
 
-(let [{:keys [chsk ch-recv send-fn state]}
-      (sente/make-channel-socket! "/ws" ?csrf-token {:type :auto})]
+
+(let [
+      {:keys [chsk ch-recv send-fn state]}
+      (sente/make-channel-socket-client! 
+       "/ws" 
+       ?csrf-token 
+       {:type :auto})]
   (def chsk       chsk)
   (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
   (def chsk-send! send-fn) ; ChannelSocket's send API fn
   (def chsk-state state)   ; Watchable, read-only atom
   )
-
-(def router (atom nil))
 
 (defn handshake-handler [{:keys [?data]}]
   (.log js/console "Connection established: " ?data))
@@ -28,7 +31,7 @@
 (defn default-event-handler [ev]
   (.log js/console (str "Unhandled event: " (:event ev))))
 
-(defn event-message-handler [& [{:keys [message state handshake] :or {state state-handler handshake handshake-handler}}]]
+(defn register-handlers [& [{:keys [message state handshake] :or {state state-handler handshake handshake-handler}}]]
   (fn [ev-msg]
     (case (:id ev-msg)
       :chsk/handshake (handshake ev-msg)
@@ -36,12 +39,12 @@
       :chsk/recv (message ev-msg)
       (default-event-handler ev-msg))))
 
-(defn stop-router []
-  (when-let [stop-fn @router] (stop-fn)))
-
+(defonce router (atom nil))
+(defn stop-router [] (when-let [stop-fn @router] (stop-fn)))
 (defn start-router [message-handler]
   (stop-router)
-  (reset! router (sente/start-chsk-router! ch-chsk (event-message-handler {:message message-handler
-                                                                           :state state-handler
-                                                                           :handshake handshake-handler}))))
+  (reset! router 
+          (sente/start-client-chsk-router! ch-chsk (register-handlers {:message message-handler
+                                                                       :state state-handler
+                                                                       :handshake handshake-handler}))))
 
