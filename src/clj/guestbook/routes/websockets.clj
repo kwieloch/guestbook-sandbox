4a6d3d51f556b02ba9e6bf1d053f040fbbb70889
Karol Wieloch <kwieloch@gmail.com>
2019-01-24 11:40:18 +0100

    reformat sente setup

--- src/clj/guestbook/routes/websockets.clj
+++ src/clj/guestbook/routes/websockets.clj
@@ -9,13 +9,13 @@
             [mount.core :refer [defstate]]
             [taoensso.sente.server-adapters.immutant :refer (get-sch-adapter)]))
 
-(let [{:keys [ch-recv
+(let [{:keys [ajax-post-fn
+              ajax-get-or-ws-handshake-fn
+              ch-recv
               send-fn 
-              connected-uids
-              ajax-post-fn 
-              ajax-get-or-ws-handshake-fn]}
+              connected-uids 
+              ]}
       (sente/make-channel-socket! (get-sch-adapter) {})]
-
   (def ring-ajax-post                ajax-post-fn)
   (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
   (def ch-chsk                       ch-recv)
